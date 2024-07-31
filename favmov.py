2c824273abcb66b8ba41ffe458a8a8bf6255899d
jordani <jordan268@pop-os.myfiosgateway.com>
2024-07-30 21:33:27 -0400

    learning python lists

---
+++ favmov.py
@@ -0,0 +1,13 @@
+favmovlist = []
+print("What are your 4 favorite movies")
+num = 1
+
+while len(favmovlist) < 5:
+    favmovadd = input(f"What is your number {num} favorite movie? ")
+    favmovlist.append(favmovadd)
+    num += 1 
+    if len(favmovlist) == 4:
+        break
+
+print(favmovlist)
+print(f"Your favorite movies are {favmovlist[0]}, {favmovlist[1]}, {favmovlist[2]}, and {favmovlist[3]}.")
